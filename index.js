0ce32b1077c90cd97ecce402530ac18c0c8f69cc
DavidWaidi <126305420+DavidWaidi@users.noreply.github.com>
2023-05-02 01:37:04 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+function  Previous() {
+    count -= 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+ let set = ["2nd set:","3rd set:","4th set:","5th set:"]
+ set = saveEl
